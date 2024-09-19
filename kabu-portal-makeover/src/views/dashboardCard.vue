e3972e6acdc056733f784b9c834e665487e89b21
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-19 14:11:25 +0300

    update

--- kabu-portal-makeover/src/views/dashboardCard.vue
+++ kabu-portal-makeover/src/views/dashboardCard.vue
@@ -14,7 +14,7 @@ function toggleDropdown() {
         <span class="icon icon-profile"></span>
         <span class="profile-name">REUBEN RICHARD NZEMBEI &#x25BC;</span>
         <div class="dropdown-menu" id="dropdownMenu" v-if="isDropdownVisible">
-          <a href="#" class="dropdown-item"> <span class="icon icon-profile"></span>Profile</a>
+          <a href="#" class="dropdown-item"> <span class="icon icon-user"></span>Profile</a>
           <a href="#" class="dropdown-item"> <span class="icon icon-logout"></span>Logout</a>
         </div>
       </div>
@@ -146,24 +146,8 @@ h3 {
   border-radius: 50%;
   margin-right: 10px;
 }
-.icon-profile {
+.icon-user{
     background-image: url("../assets/img/profile.png");
-    width: 24px;  /* Adjust size as needed */
-    height: 24px; /* Adjust size as needed */
-    display: inline-block;
-    background-size: contain; /* Ensures the image fits the span */
-    background-repeat: no-repeat;
-    background-position: center;
-}
-
-.icon-logout {
-    background-image: url("../assets/img/UI.png");
-    width: 24px;  /* Adjust size as needed */
-    height: 24px; /* Adjust size as needed */
-    display: inline-block;
-    background-size: contain; /* Ensures the image fits the span */
-    background-repeat: no-repeat;
-    background-position: center;
 }
 
 /* Dashboard Container */
